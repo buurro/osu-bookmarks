1b6a0e0581ba09775d0a3f022e58f8a1799c2464
Marco Burro <marcoburro98@gmail.com>
2020-01-28 19:07:36 +0100

    Add Beatmap to Admin

--- songs/admin.py
+++ songs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Song, BeatmapSet
+from .models import Song, BeatmapSet, Beatmap
 
 
 admin.site.register(Song)
 admin.site.register(BeatmapSet)
+admin.site.register(Beatmap)
